60ab8e61bec7dda667c4850e2ef310bc84616740
StewartOjedaR <stewart.ojeda@udea.edu.co>
2026-04-14 11:19:57 -0500

    Funciones corregidas

--- FuncCC.h
+++ FuncCC.h
@@ -4,59 +4,75 @@
 #include <fstream>
 using namespace std;
 // Función para redimensionar el arreglo
-void redimensionar(char*& arreglo,unsigned int& capacidad,unsigned int nuevaCapacidad,unsigned int contador) {
+void redimensionar(char*& arreglo,unsigned int capacidad,unsigned int nuevaCapacidad,unsigned int contador) {
     char* pArregloCopia = new char[nuevaCapacidad];
     
     // Copiar elementos existentes
     for (unsigned int i = 0; i < contador; i++) {
         pArregloCopia[i] = arreglo[i];
     }
-    delete[] arreglo;
+  //  delete[] arreglo;
+    arreglo = nullptr;
     arreglo = pArregloCopia;
     capacidad = nuevaCapacidad;
 }
 
 
-void ImprimirCadenaRLE(char *CadenaComprimida, char *CaracRepetidos,unsigned short NuevoContador){
-for  (int i=0; i<NuevoContador; i++){
+void ImprimirCadenaRLE(char *CadenaComprimida, char *CaracRepetidos){
+int contador = 0;
+for (int i =0; i<i+1; i++){
+if (CadenaComprimida[i]== '\0') break;
+contador++;
+}
+
+for  (int i=0; i<contador; i++){
     cout << CadenaComprimida[i]<<int(CaracRepetidos[i]);
 }}
 
 
-void leerTexto(char*& Cadena, unsigned int& contador,unsigned int& capacidad,ifstream& entrada, const char* mensaje = "Ingresa texto (Enter para terminar): ") {
-    if (capacidad == 0) capacidad = 1;
-    Cadena = new char[capacidad];
-    contador = 0;
+void ArregloString(char*& Cadena,istream& entrada, const char* mensaje = "Ingresa texto (Enter para terminar): ") {
+    int Capacidad = 1;
+    int*ptrCapacidad=&Capacidad;
+    Cadena = new char[Capacidad];
+    unsigned int contador = 0;
     char Caracter;
     if (&entrada == &cin) cout << mensaje;
         while (entrada.get(Caracter)) {                               // Lee un caracter a la vez, el bucle continúa hasta que se presiona Enter (caracter de nueva línea)
         if (&entrada == &cin && Caracter == '\n'){ break;}
-        if (contador >= capacidad) {                                            //Si el contador alcanza o supera la capacidad actual del arreglo, es necesario redimensionar el arreglo para acomodar más caracteres
-            redimensionar(Cadena, capacidad, capacidad +1, contador);          // Copia el viejo arreglo, libera el viejo arreglo y crea uno nuevo con la nueva capacidad
+        if (contador >= Capacidad) {                                            //Si el contador alcanza o supera la capacidad actual del arreglo, es necesario redimensionar el arreglo para acomodar más caracteres
+            redimensionar(Cadena, Capacidad, Capacidad +1, contador);          // Copia el viejo arreglo, libera el viejo arreglo y crea uno nuevo con la nueva capacidad
+            Capacidad++; // Incrementa la capacidad para la proxima vez que se necesite redimensionar el arreglo, en este caso se incrementa de uno en uno para evitar desperdiciar memoria
         }
         Cadena[contador++] = Caracter;                                          // Almacena el caracter ingresado en el arreglo y luego incrementa el contador para la próxima posición del arreglo
     }
     
     // Agregar el caracter nulo al final
-    if (contador >= capacidad) {
-        redimensionar(Cadena, capacidad, capacidad + 1, contador);
+    if (contador >= Capacidad) {
+        redimensionar(Cadena, Capacidad, Capacidad + 1, contador);
     } 
     Cadena[contador] = '\0';
 }
 
 
-void comprimirRLE(const char* Cadena, unsigned int contador, char*& CadenaComprimida, char*& CaracRepetidos, unsigned int& NuevoContador,unsigned int& capacidad) {
-    capacidad = 0;
-    NuevoContador = 0;
+void comprimirRLE(const char* Cadena, char*& CadenaComprimida, char*& CaracRepetidos) {
+int  capacidad = 0;
+int contador = 0;
+for (int i =0; i<i+1; i++){
+if (Cadena[i]== '\0') break;
+contador++;
+}
+
+    int NuevoContador = 0;
     unsigned short repeticiones = 1;
     
-    CadenaComprimida = new char[capacidad];
-    CaracRepetidos = new char[capacidad];
+ //   CadenaComprimida = new char[capacidad];
+ //   CaracRepetidos = new char[capacidad];
     
       for (int i=0; i < contador; i++){                                           //contardor para recorrer el arreglo de caracteres
         if (NuevoContador>=capacidad){
             redimensionar(CadenaComprimida, capacidad, capacidad+1, NuevoContador); //redimensiono el arreglo comprimido si el contador alcanza o supera la capacidad actual del arreglo
             redimensionar(CaracRepetidos, capacidad, capacidad+1, NuevoContador);
+            capacidad++;
         }
         if ( (i+1) < contador &&Cadena[i]==Cadena[i+1]){
            repeticiones++; continue; //si el caracter actual es igual al siguiente, incremento el contador de repeticiones y continuo con la siguiente iteracion del bucle para seguir contando las repeticiones de ese caracter 
@@ -68,15 +84,25 @@ void comprimirRLE(const char* Cadena, unsigned int contador, char*& CadenaCompri
             repeticiones=1;
         }
     }
+    if (contador >= capacidad) {
+        redimensionar(CadenaComprimida, capacidad, capacidad + 1, contador);
+        redimensionar(CaracRepetidos, capacidad, capacidad + 1, contador);
+    } 
+    CadenaComprimida[contador] = '\0';
 }
 
 
 
 // Función para descomprimir RLE
 void descomprimirRLE(const char* CadenaComprimida, const char* CaracRepetidos) {
-  for (int i = 0; i < sizeof(CadenaComprimida); i++) {
+int contador = 0;
+for (int i =0; i<i+1; i++){
+if (CaracRepetidos[i]== '\0') break;
+contador++;
+}
+  for (int i = 0; i < contador; i++) {
     for (int j = 0; j < int(CaracRepetidos[i]); j++) {
-      cout << CadenaComprimida[i];
+       cout << CadenaComprimida[i];
     }
   }
 }
@@ -93,9 +119,16 @@ for (int i=(sizeof(a)*8)-1;i>=0;i--){
 }
 
 
-void RotarBits(char *CadenaComprimida, unsigned int contador, unsigned int CantidadDeRotaciones){
-    for (int j=0; j<contador; j++){
-    bin(CadenaComprimida[j]);
+void RotarBits(char *CadenaComprimida, unsigned int CantidadDeRotaciones){
+int contador = 0;
+for (int i =0; i<i+1; i++){
+if (CadenaComprimida[i]== '\0') break;
+contador++;
+}
+
+
+   for (int j=0; j<contador; j++){
+  //  bin(CadenaComprimida[j]);
  //   cout<<"Byte"<<endl;
 
     for (int i=0;i<CantidadDeRotaciones;i++){
--- main.cpp
+++ main.cpp
@@ -2,60 +2,30 @@
 #include <fstream>
 #include "FuncCC.h"
 using namespace std;
-unsigned int Capacidad=0;
     char* Cadena = nullptr;                                        //un arreglo dinamico para almacenar los caracteres incluyendo los espacios, el arreglo se redimensionará según sea necesario
-    unsigned int contador = 0;   
-    unsigned int NuevoContador=0;                                   //para el nuevo arreglo comprimido
     char*CadenaComprimida = nullptr;                                //arreglo donde almacenare la nueva cadena comrpimida, se redimencionara segun sea necesario
     char*CaracRepetidos = nullptr;
-
 int main() {
 std::ifstream archivo("XD.txt");                                    //leer un archivo de texto, se utiliza la clase ifstream para abrir el archivo y se pasa como argumento a la funcion leerTexto, el archivo debe estar en la misma carpeta que el programa o se debe especificar la ruta completa del archivo
     if (archivo.is_open()) {
-        leerTexto(Cadena, contador, Capacidad, archivo);
-/*for (int i=0; i<contador; i++){
-    cout<<Cadena[i];
-    }*/
+        ArregloString(Cadena, archivo);
         archivo.close();
-        delete[] Cadena;//libero memoria del arreglo original ya que no se necesita mas
     } else {
         cout << "No se pudo abrir el archivo." << endl;
     }
-//    leerTexto(Cadena,contador,Capacidad,cin);                                                //funcion que lee bien sea un texto de entrada o un archivo .txt, el texto se almacena en un arreglo dinamico que se redimensiona segun sea necesario, el contador se utiliza para llevar un registro de la cantidad de caracteres ingresados y la capacidad se utiliza para determinar cuándo es necesario redimensionar el arreglo
-    comprimirRLE(Cadena,contador,CadenaComprimida,CaracRepetidos,NuevoContador,Capacidad);  //Funcion que comprime la cadena utilizando el algoritmo RLE, se recorre el arreglo de caracteres y se cuenta cuantas veces se repite cada caracter, se almacena el caracter comprimido en un nuevo arreglo y la cantidad de repeticiones en otro arreglo, ambos arreglos se redimensionan segun sea necesario, el nuevo contado se utiliza para llevar un registro de la cantidad de caracteres comprimidos y la capacidad se utiliza para determinar cuando sea necesario redimencionar los arreglos
-//    delete[] Cadena;//libero memoria del arreglo original ya que no se necesita mas
-    ImprimirCadenaRLE(CadenaComprimida, CaracRepetidos, NuevoContador);                        //funcion que imprime la cadena comprimida, se recorre el arreglo de caracteres cmprimidos y se imprime cada caracter seguido de la cantidad de repeticiones, se utiliza el nuevo contador para determinar la cantidad de caracteres comprimidos a imprimir
-  //  descomprimirRLE(CadenaComprimida, CaracRepetidos);                                      //funcion qque descomprime la cadena comprimida, se recorre el arreglo de caracteres comprimidos y se imprime cada caracter repetido la cantidad de veces indicada en el arreglo de repeticiones
+//    ArregloString(Cadena, cin);                                                //funcion que lee bien sea un texto de entrada o un archivo .txt, el texto se almacena en un arreglo dinamico que se redimensiona segun sea necesario, el contador se utiliza para llevar un registro de la cantidad de caracteres ingresados y la capacidad se utiliza para determinar cuándo es necesario redimensionar el arreglo
+    comprimirRLE(Cadena,CadenaComprimida,CaracRepetidos);  //Funcion que comprime la cadena utilizando el algoritmo RLE, se recorre el arreglo de caracteres y se cuenta cuantas veces se repite cada caracter, se almacena el caracter comprimido en un nuevo arreglo y la cantidad de repeticiones en otro arreglo, ambos arreglos se redimensionan segun sea necesario, el nuevo contado se utiliza para llevar un registro de la cantidad de caracteres comprimidos y la capacidad se utiliza para determinar cuando sea necesario redimencionar los arreglos
+    delete[] Cadena;//libero memoria del arreglo original ya que no se necesita mas
+    //ImprimirCadenaRLE(CadenaComprimida, CaracRepetidos, NuevoContador);                        //funcion que imprime la cadena comprimida, se recorre el arreglo de caracteres cmprimidos y se imprime cada caracter seguido de la cantidad de repeticiones, se utiliza el nuevo contador para determinar la cantidad de caracteres comprimidos a imprimir
+    descomprimirRLE(CadenaComprimida, CaracRepetidos);                                      //funcion qque descomprime la cadena comprimida, se recorre el arreglo de caracteres comprimidos y se imprime cada caracter repetido la cantidad de veces indicada en el arreglo de repeticiones
+  //  ImprimirCadenaRLE(CadenaComprimida, CaracRepetidos); 
     int CantidadDeRotaciones;                                                                //variable para almacenar la cantidad de rotaciones que el usuario desea realizar en los bits de los caracteres comprimidos 
-/*for (int k=0; k<NuevoContador; k++){                                                        //ciclo for para imprimir los bits de cada caractercomrpimido, se utilio como ayuda visual
-bin(CadenaComprimida[k]);
-cout<<endl;
-}
-*/
     do                                                                                //ciclo do-while para solicitar al usuario la cantidad de rotaciones a realizar en los bits de los caracteres comprimidos, se valida que el numero ingresado sea menor a 8 ya que un byte tiene 8 bits y no se pueden rotar maas de 7 posiciones sin volver al estado original
     {
         cout<<"\nIngrese un numero menor a 8 para las rotaciones: "<<endl;                //
         cin>>CantidadDeRotaciones;                                                       // 
     } while (CantidadDeRotaciones>7);                                                  //condion para validar que el numero ingresado sea menor a 8
-RotarBits(CadenaComprimida, NuevoContador, CantidadDeRotaciones);                      //funcion que rota los bits de cada caracter comprimido, se recorre el arreglo de cada caracter comprimido y se realiza la rotacion de bits segun la cantidad de rotaciones indicada por el usuario, se utiliza el nuevo contador para determinar la cantidad de caracteres comprimidos a rotar
- /*   for (int j=0; j<contador; j++){// ciclo for para ver cada byte del arreglo, solo utilizado como ayuda visual para verificar que la funcion de rotacion de bits este funcionando correctamente
-
-    bin(CadenaComprimida[j]);
-    cout<<"Byte"<<endl;
-
-    for (int i=0;i<CantidadDeRotaciones;i++){
-        if ((CadenaComprimida[j]&1<<7)){ 
-            CadenaComprimida[j]<<=1;
-            CadenaComprimida[j]|=1;
-            bin(CadenaComprimida[j]);
-            cout<<endl;
-            continue;
-        }
-        CadenaComprimida[j]<<=1;
-            bin(CadenaComprimida[j]);
-            cout<<endl;
-    }
-    }*/
+RotarBits(CadenaComprimida, CantidadDeRotaciones);                      //funcion que rota los bits de cada caracter comprimido, se recorre el arreglo de cada caracter comprimido y se realiza la rotacion de bits segun la cantidad de rotaciones indicada por el usuario, se utiliza el nuevo contador para determinar la cantidad de caracteres comprimidos a rotar
 
 
 
@@ -79,5 +49,34 @@ RotarBits(CadenaComprimida, NuevoContador, CantidadDeRotaciones);               
 
 
 
+
+
         return 0;
 }
+
+
+ /*   for (int j=0; j<contador; j++){// ciclo for para ver cada byte del arreglo, solo utilizado como ayuda visual para verificar que la funcion de rotacion de bits este funcionando correctamente
+
+    bin(CadenaComprimida[j]);
+    cout<<"Byte"<<endl;
+
+    for (int i=0;i<CantidadDeRotaciones;i++){
+        if ((CadenaComprimida[j]&1<<7)){ 
+            CadenaComprimida[j]<<=1;
+            CadenaComprimida[j]|=1;
+            bin(CadenaComprimida[j]);
+            cout<<endl;
+            continue;
+        }
+        CadenaComprimida[j]<<=1;
+            bin(CadenaComprimida[j]);
+            cout<<endl;
+    }
+    }*/
+
+
+/*for (int k=0; k<NuevoContador; k++){                                                        //ciclo for para imprimir los bits de cada caractercomrpimido, se utilio como ayuda visual
+bin(CadenaComprimida[k]);
+cout<<endl;
+}
+*/
